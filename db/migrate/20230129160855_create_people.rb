299e1f267d97ad129dd5089ec5f7be556515bf58
Igor Zubkov <igor.zubkov@gmail.com>
2023-01-29 17:09:42 +0100

    Add migration

---
+++ db/migrate/20230129160855_create_people.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class CreatePeople < ActiveRecord::Migration[7.0]
+  def change
+    create_table :people do |t|
+      t.string :name
+      t.string :email
+      t.string :phone
+
+      t.timestamps
+    end
+  end
+end
