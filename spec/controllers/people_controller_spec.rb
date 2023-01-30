2bb5e2c237e7fd77daf18c9058816a1ec928e55b
Igor Zubkov <igor.zubkov@gmail.com>
2023-01-30 01:21:37 +0100

    PeopleController#destroy

--- spec/controllers/people_controller_spec.rb
+++ spec/controllers/people_controller_spec.rb
@@ -19,4 +19,37 @@ describe PeopleController do
 
     it { should render_template(:index) }
   end
+
+  describe "#destroy" do
+    let(:person) { instance_double(Person) }
+
+    before do
+      #
+      # Person.find(params[:id]) # => person
+      #
+      expect(Person).to receive(:find).with("1").and_return(person)
+    end
+
+    before { expect(person).to receive(:destroy!) }
+
+    context "when format turbo stream" do
+      before { delete :destroy, params: {id: "1", format: "turbo_stream"} }
+
+      it { should respond_with(:ok) }
+
+      it { should render_template(:destroy) }
+
+      it { should set_flash.now[:notice].to("Person was successfully destroyed.") }
+    end
+
+    context "when format html" do
+      before { delete :destroy, params: {id: "1", format: "html"} }
+
+      it { should respond_with(:see_other) }
+
+      it { should redirect_to(people_path) }
+
+      it { should set_flash[:notice].to("Person was successfully destroyed.") }
+    end
+  end
 end
