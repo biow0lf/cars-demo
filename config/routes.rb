7eb407945b7d2c134429edfa1cd18cb44a02e362
Igor Zubkov <igor.zubkov@gmail.com>
2023-01-31 21:46:48 +0100

    Add routes and tests

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@
 Rails.application.routes.draw do
   resources :people
 
-  resources :cars, only: :index
+  resources :cars
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
--- spec/routing/cars_routing_spec.rb
+++ spec/routing/cars_routing_spec.rb
@@ -10,5 +10,59 @@ describe CarsController do
         action: "index"
       )
     end
+
+    it "should route GET /cars/new to cars#new" do
+      expect(get: "/cars/new").to route_to(
+        controller: "cars",
+        action: "new"
+      )
+    end
+
+    it "should route GET /cars/1 to cars#show" do
+      expect(get: "/cars/1").to route_to(
+        controller: "cars",
+        action: "show",
+        id: "1"
+      )
+    end
+
+    it "should route GET /cars/1/edit to cars#edit" do
+      expect(get: "/cars/1/edit").to route_to(
+        controller: "cars",
+        action: "edit",
+        id: "1"
+      )
+    end
+
+    it "should route POST /cars to cars#create" do
+      expect(post: "/cars").to route_to(
+        controller: "cars",
+        action: "create"
+      )
+    end
+
+    it "should route PUT /cars/1 to cars#update" do
+      expect(put: "/cars/1").to route_to(
+        controller: "cars",
+        action: "update",
+        id: "1"
+      )
+    end
+
+    it "should route PATCH /cars/1 to cars#update" do
+      expect(patch: "/cars/1").to route_to(
+        controller: "cars",
+        action: "update",
+        id: "1"
+      )
+    end
+
+    it "should route DELETE /cars/1 to cars#destroy" do
+      expect(delete: "/cars/1").to route_to(
+        controller: "cars",
+        action: "destroy",
+        id: "1"
+      )
+    end
   end
 end
