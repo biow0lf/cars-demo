dd8e5fde0d4bea563b9df2730a45dfa34b87b777
Igor Zubkov <igor.zubkov@gmail.com>
2023-01-31 22:32:45 +0100

    Update cars controller

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -14,14 +14,43 @@ class CarsController < ApplicationController
   end
 
   def edit
+    @car = Car.find(params[:id])
   end
 
-  def create
-  end
+  # def create
+  #   @car = Car.new(car_params)
+  #
+  #   if @car.save
+  #     redirect_to car_path(@car), notice: t(".successful")
+  #   else
+  #     render :new, status: :unprocessable_entity
+  #   end
+  # end
+  #
+  # def update
+  #   @car = Car.find(params[:id])
+  #
+  #   if @car.update(car_params)
+  #     redirect_to car_path(@car), notice: t(".successful")
+  #   else
+  #     render :edit, status: :unprocessable_entity
+  #   end
+  # end
+  #
+  # def destroy
+  #   @car = Car.find(params[:id])
+  #   @car.destroy!
+  #
+  #   respond_to do |format|
+  #     format.turbo_stream { flash.now[:notice] = t(".successful") }
+  #     format.html { redirect_to cars_path, status: :see_other, notice: t(".successful") }
+  #   end
+  # end
 
-  def update
-  end
+  private
 
-  def destroy
+  def car_params
+    # TODO: person_id
+    params.require(:car).permit(:model, :make, :color, :mileage, :for_sale)
   end
 end
