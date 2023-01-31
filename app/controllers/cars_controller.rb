d89358ad9aeeda25af78ba9e27044b8f86603f92
Igor Zubkov <igor.zubkov@gmail.com>
2023-01-31 21:53:02 +0100

    Cars#new

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -4,4 +4,24 @@ class CarsController < ApplicationController
   def index
     @cars = Car.page(params[:page])
   end
+
+  def show
+    @car = Car.find(params[:id])
+  end
+
+  def new
+    @car = Car.new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
