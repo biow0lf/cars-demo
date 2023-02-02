f8320ee65594daeff4a7dfcd7a3a37cbe1dbac1d
Igor Zubkov <igor.zubkov@gmail.com>
2023-02-02 11:40:13 +0100

    Add controller tests

--- spec/controllers/car_ownerships_history_controller_spec.rb
+++ spec/controllers/car_ownerships_history_controller_spec.rb
@@ -6,6 +6,17 @@ describe CarOwnershipsHistoryController do
   it { should be_an(ApplicationController) }
 
   describe "#index" do
+    before do
+      #
+      # Car.includes(ownerships: :person).find(params[:car_id])
+      #
+      expect(Car).to receive(:includes).with(ownerships: :person) do
+        double.tap do |a|
+          expect(a).to receive(:find).with("1")
+        end
+      end
+    end
+
     before { get :index, params: {car_id: "1"} }
 
     it { should respond_with(:ok) }
