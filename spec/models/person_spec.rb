e783f9792a87dd6d163037c1ab86c0549fb970f8
Igor Zubkov <igor.zubkov@gmail.com>
2023-02-02 10:17:51 +0100

    Person#ownerships

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -6,4 +6,6 @@ describe Person do
   it { should be_an(ApplicationRecord) }
 
   it { should have_many(:cars).dependent(:destroy) }
+
+  it { should have_many(:ownerships).dependent(:destroy) }
 end
