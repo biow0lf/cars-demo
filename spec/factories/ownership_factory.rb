dd61771e512cb4cc5074e1b95bbb381c7053f6bf
Igor Zubkov <igor.zubkov@gmail.com>
2023-02-02 12:12:49 +0100

    Update ownership factory

--- spec/factories/ownership_factory.rb
+++ spec/factories/ownership_factory.rb
@@ -5,7 +5,7 @@ FactoryBot.define do
     car
     person
     date_of_sale { "2023-02-01" }
-    price { 1 }
-    mileage { 1 }
+    price { [100, 1_000, 2_000, 5_000, 10_000].sample }
+    mileage { [50_000, 100_000, 150_000].sample }
   end
 end
