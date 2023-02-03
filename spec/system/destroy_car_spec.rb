b6f22a92e4ebf151ff28a3864ddabd830b507ab9
Igor Zubkov <igor.zubkov@gmail.com>
2023-02-03 20:19:11 +0100

    More tests

--- spec/system/destroy_car_spec.rb
+++ spec/system/destroy_car_spec.rb
@@ -3,4 +3,21 @@
 require "rails_helper"
 
 describe "Destroy car features" do
+  describe "#index" do
+    it "should destroy car" do
+      create(:car)
+
+      expect(Car.count).to eq(1)
+
+      visit cars_path
+
+      accept_confirm do
+        click_link "Destroy"
+      end
+
+      page.assert_text "Car was successfully destroyed."
+
+      expect(Car.count).to eq(0)
+    end
+  end
 end
