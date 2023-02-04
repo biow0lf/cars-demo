34343de32e96d26447614feda752cb4d6a956193
Igor Zubkov <igor.zubkov@gmail.com>
2023-02-04 15:19:18 +0100

    Add tests

---
+++ spec/controllers/sells_controller_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe SellsController do
+  it { should be_an(ApplicationController) }
+
+  describe "#new"
+
+  describe "#create"
+end
