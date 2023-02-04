48fdbf9163dd86e80d1f904f8fea5007ee0e05c7
Igor Zubkov <igor.zubkov@gmail.com>
2023-02-04 15:22:39 +0100

    More tests

--- spec/controllers/sells_controller_spec.rb
+++ spec/controllers/sells_controller_spec.rb
@@ -5,7 +5,20 @@ require "rails_helper"
 describe SellsController do
   it { should be_an(ApplicationController) }
 
-  describe "#new"
+  describe "#new" do
+    before { expect(Car).to receive(:find).with("2") }
 
-  describe "#create"
+    before { expect(Person).to receive(:pluck).with(:name, :id) }
+
+    before { expect(Ownership).to receive(:new) }
+
+    before { get :new, params: {car_id: "2"} }
+
+    it { should respond_with(:ok) }
+
+    it { should render_template(:new) }
+  end
+
+  describe "#create" do
+  end
 end
