feabf3c90f298aa8d3887194d2dd3d971a070da5
Igor Zubkov <igor.zubkov@gmail.com>
2023-02-04 14:35:54 +0100

    Sell

--- app/controllers/sells_controller.rb
+++ app/controllers/sells_controller.rb
@@ -4,8 +4,19 @@ class SellsController < ApplicationController
   def new
     @car = Car.find(params[:car_id])
     @people = Person.pluck(:name, :id)
+    @ownership = Ownership.new
   end
 
   def create
+    @car = Car.find(params[:car_id])
+    @people = Person.pluck(:name, :id)
+    @person = Person.find(params.dig(:ownership, :person_id))
+    @ownership = Ownership.new(car: @car, person: @person)
+
+    if @ownership.save
+      redirect_to car_path(@car), notice: t(".successful")
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 end
