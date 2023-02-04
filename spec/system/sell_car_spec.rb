a04c74b811b38286a1649512d2306deb5c267823
Igor Zubkov <igor.zubkov@gmail.com>
2023-02-04 12:44:55 +0100

    Update system test

--- spec/system/sell_car_spec.rb
+++ spec/system/sell_car_spec.rb
@@ -4,6 +4,10 @@ require "rails_helper"
 
 describe "Sell car features" do
   it "should change car owner" do
+    car = create(:car)
 
+    visit car_sell_path(car)
+
+    # TODO: check sell button
   end
 end
