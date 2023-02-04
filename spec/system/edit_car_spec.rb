cbb19371d19328d540fd1b4bc953372de5c8101a
Igor Zubkov <igor.zubkov@gmail.com>
2023-02-04 15:12:17 +0100

    Update system test

--- spec/system/edit_car_spec.rb
+++ spec/system/edit_car_spec.rb
@@ -4,22 +4,16 @@ require "rails_helper"
 
 describe "Update car features" do
   it "should update car" do
-    person_1 = create(:person, name: "Jane Snow")
-    person_2 = create(:person, name: "John Snow")
-
     car = create(:car,
       make: "Tesla",
       model: "Model 3",
       color: "black",
       mileage: 10_000,
-      owner: person_1,
       for_sale: false)
 
     expect(Car.count).to eq(1)
 
     visit edit_car_path(car)
-
-    # binding.pry
 
     fill_in "Car make", with: "Toyota"
 
@@ -30,8 +24,6 @@ describe "Update car features" do
     fill_in "Car mileage", with: "20000"
 
     check "Car for sale?"
-
-    select person_2.name, from: "car_person_id"
 
     click_button "Update Car"
 
@@ -48,7 +40,5 @@ describe "Update car features" do
     expect(Car.first.mileage).to eq(20_000)
 
     expect(Car.first.for_sale).to eq(true)
-
-    expect(Car.first.person_id).to eq(person_2.id)
   end
 end
