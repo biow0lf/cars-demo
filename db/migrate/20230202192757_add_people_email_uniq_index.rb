e4a27522efffbbf0e43f49c6c0e1389374275e8f
Igor Zubkov <igor.zubkov@gmail.com>
2023-02-05 13:41:15 +0100

    Update migration

--- db/migrate/20230202192757_add_people_email_uniq_index.rb
+++ db/migrate/20230202192757_add_people_email_uniq_index.rb
@@ -2,6 +2,6 @@
 
 class AddPeopleEmailUniqIndex < ActiveRecord::Migration[7.0]
   def change
-    add_index :people, "lower(email)", unique: true
+    add_index :people, :email, unique: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -44,7 +44,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_02_192757) do
     t.string "phone"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index "lower((email)::text)", name: "index_people_on_lower_email", unique: true
+    t.index ["email"], name: "index_people_on_email", unique: true
   end
 
   add_foreign_key "cars", "people"
